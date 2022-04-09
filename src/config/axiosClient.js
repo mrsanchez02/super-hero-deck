1f53a46ce6ba6d6dc1f788e8f51fabdcaa45291a
Mr Jose Sanchez <leandrosanchez.it@gmail.com>
2022-04-08 21:48:46 -0400

    Cors origins issue

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosClient =  axios.create({
-    baseURL : 'https://challenge-react.alkemy.org/'
+    baseURL : 'https://challenge-react.alkemy.org/',
+    headers: {'Access-Control-Allow-Origin': '*'}
 });
 
 export default axiosClient;
